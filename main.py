a5994b7ddd0b9513becc47a287ddb133f7bdc9a6
ifcassianasl <cassianasilveira64@gmail.com>
2023-01-03 20:36:38 -0300

    trabalho

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ def df(h):
 def fx(x):
   return x**3 - 9*x + 3
 
+def qx(h):
+  return ((-1.24 + 0.5*np.pi - np.arcsin(h)) / np.sqrt(1-h**2))
+
 # Método da bissecção
 def bissecao(f, a, b, tol):
   i = 1
@@ -19,7 +22,7 @@ def bissecao(f, a, b, tol):
       p = (a + b) / 2 
       fp = f(p)
     if ((abs(fp) < tol) or (b - a < tol)):  
-      return (p, i, fp, (b-a)/2)
+      return (p, i, fp, a, b, b-a)
     
     i = i+1
     if f(a) * fp < 0:
@@ -41,7 +44,7 @@ def falsa_posicao(f, a, b, tol):
       fp = f(p)
 
     if ((abs(fp) < tol) or (b - a < tol)):  
-      return (p, i, fp, (b-a)/2)
+      return (p, i, fp, (b-a))
     
     i = i+1
     if fa * fp < 0:
@@ -54,5 +57,59 @@ def falsa_posicao(f, a, b, tol):
 print('falsa posicao', falsa_posicao(f, 0, 1, 1e-2))
 
 # Método do ponto fixo
+def ponto_fixo(f, qx, x0, tol):
+  i = 1
+  while True:
+    x1 = qx(x0)
+    fx1 = f(x1)
+    print(x1)
+    if ((abs(fx1) < tol)):  
+      return (x1, i, fx1, (x1 - x0)/2)
+
+    x0=x1
+    i = i+1
+
+#print('ponto fixo', ponto_fixo(f, qx, 0.15625, 1e-2)) 
+# na iteração, x nao pode ser  1, pois anula a divisao
+
 # Método de Newton
+# Erro 
+def newton(f, df, x0, tol):
+  i = 1
+  while True:
+    fx0 = f(x0)
+    dfx0 = df(x0)
+    
+    x1 = (x0 - (fx0/dfx0))
+    fx1 = f(x1)
+    
+    if ((abs(fx1) < tol)):  
+      return (x1, i, fx1, (x1-x0)/2)
+    
+    x0 = x1
+    i = i + 1
+
+print('newton', newton(f, df, 0, 1e-2))
+
 # Método da secante
+def secante(f, x1, x0, tol):
+  i = 1
+  while True:
+    fx0 = f(x0)
+    fx1 = f(x1)
+    if fa * fb < 0:
+      p = (a*fb - b*fa) / (fb - fa)
+      fp = f(p)
+
+    if ((abs(fp) < tol) or (b - a < tol)):  
+      return (p, i, fp, (b-a)/2)
+    
+    i = i+1
+    if fa * fp < 0:
+      b = p
+      fb = fp
+    else:
+      a = p   
+      fa = fp
+
+#print('secante', secante(f, 0, 1, 1e-2))
