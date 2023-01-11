25cc10b42444eeb3dcc8cf0057ce745171e44498
ifcassianasl <cassianasilveira64@gmail.com>
2023-01-10 22:39:09 -0300

    funcionando

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def fx(x):
   return x**3 - 9*x + 3
 
 def qx(h):
-  return ((-1.24 + 0.5*np.pi - np.arcsin(h)) / np.sqrt(1-h**2))
+  return (np.sin(-1.24 + 0.5*np.pi - h*np.sqrt(1-h**2)))
 
 # Método da bissecção
 def bissecao(f, a, b, tol):
@@ -29,9 +29,6 @@ def bissecao(f, a, b, tol):
       b = p
     else:
       a = p   
-
-print('bissecao', bissecao(f, 0, 1, 1e-2))
-
 
 # Método da falsa posição
 def falsa_posicao(f, a, b, tol):
@@ -54,26 +51,20 @@ def falsa_posicao(f, a, b, tol):
       a = p   
       fa = fp
 
-print('falsa posicao', falsa_posicao(f, 0, 1, 1e-2))
-
 # Método do ponto fixo
-def ponto_fixo(f, qx, x0, tol):
+def ponto_fixo(f, qx, x0, tol, n):
   i = 1
-  while True:
+  while i <= n:
     x1 = qx(x0)
     fx1 = f(x1)
-    print(x1)
-    if ((abs(fx1) < tol)):  
-      return (x1, i, fx1, (x1 - x0)/2)
+    if ((abs(fx1) < tol)  or (abs(x1 - x0) < tol)):  
+      return (x1, i, fx1, (x1 - x0))
 
-    x0=x1
+    x0 = x1
     i = i+1
 
-#print('ponto fixo', ponto_fixo(f, qx, 0.15625, 1e-2)) 
 # na iteração, x nao pode ser  1, pois anula a divisao
-
 # Método de Newton
-# Erro 
 def newton(f, df, x0, tol):
   i = 1
   while True:
@@ -89,27 +80,26 @@ def newton(f, df, x0, tol):
     x0 = x1
     i = i + 1
 
-print('newton', newton(f, df, 0, 1e-2))
-
 # Método da secante
 def secante(f, x1, x0, tol):
   i = 1
   while True:
     fx0 = f(x0)
     fx1 = f(x1)
-    if fa * fb < 0:
-      p = (a*fb - b*fa) / (fb - fa)
-      fp = f(p)
 
-    if ((abs(fp) < tol) or (b - a < tol)):  
-      return (p, i, fp, (b-a)/2)
+    qx = (x0*fx1 - x1*fx0) / (fx1 - fx0)
+
+    fqx = f(qx)
+
+    if ((abs(fqx) < tol) or (abs(qx - x1) < tol)):  
+      return (qx, i, fqx, abs(qx - x1))
     
     i = i+1
-    if fa * fp < 0:
-      b = p
-      fb = fp
-    else:
-      a = p   
-      fa = fp
+    x0 = x1
+    x1 = qx
 
-#print('secante', secante(f, 0, 1, 1e-2))
+print('bissecao', bissecao(f, 0, 1, 1e-2))
+print('falsa posicao', falsa_posicao(f, 0, 1, 1e-2))
+print('ponto fixo', ponto_fixo(f, qx, 0.171875, 1e-2, 300))
+print('newton', newton(f, df, 0, 1e-2))
+print('secante', secante(f, 0, 1, 1e-2))
